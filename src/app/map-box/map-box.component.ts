017b796c08c0eac829c2271faf1550226932d95f
Ariel Noyman <noyman@mit.edu>
2019-02-27 14:58:49 -0500

    modules => folders

--- src/app/map-box/map-box.component.ts
+++ src/app/map-box/map-box.component.ts
@@ -1,24 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-import * as mapboxgl from 'mapbox-gl';
-import { GridDataService } from '../grid-data.service';
-import { MobilitySimulationService } from '../mobility-simulation.service';
-import { MapService } from '../map.service';
-import { GeoJson, FeatureCollection } from '../map';
-
+import { Component, OnInit } from "@angular/core";
+import * as mapboxgl from "mapbox-gl";
+import { GridDataService } from "../grid-data/grid-data.service";
+import { MobilitySimulationService } from "../mobility-simulation/mobility-simulation.service";
+import { MapService } from "../map/map.service";
+import { GeoJson, FeatureCollection } from "../map/map";
 
 declare var Threebox: any;
 
-
 @Component({
-  selector: 'map-box',
-  templateUrl: './map-box.component.html',
-  styleUrls: ['./map-box.component.css']
+  selector: "map-box",
+  templateUrl: "./map-box.component.html",
+  styleUrls: ["./map-box.component.css"]
 })
-export class MapBoxComponent implements OnInit{
-
+export class MapBoxComponent implements OnInit {
   /// default settings
   map: mapboxgl.Map;
-  style = 'mapbox://styles/relnox/cjs9rb33k2pix1fo833uweyjd';
+  style = "mapbox://styles/relnox/cjs9rb33k2pix1fo833uweyjd";
   latitude: number;
   longitude: number;
   rotation: number;
@@ -28,68 +25,62 @@ export class MapBoxComponent implements OnInit{
   simDataSource: any;
 
   // threebox hack
-  threebox: any
-  threeGrid: any
+  threebox: any;
+  threeGrid: any;
 
   constructor(
-  	private mapService: MapService,
-  	private gridDataService: GridDataService,
-    private mobilitySimulationService: MobilitySimulationService) {
-  }
+    private mapService: MapService,
+    private gridDataService: GridDataService,
+    private mobilitySimulationService: MobilitySimulationService
+  ) {}
 
   ngOnInit() {
-
-    this.gridDataService.getTableData()
-      .subscribe(_data => {
-        this.latitude = this.gridDataService.getLatitude()
-      	this.longitude = this.gridDataService.getLongitude()
-      	this.rotation = (-1)*this.gridDataService.getRotation()
-        this.initializeMap()
-      });
+    this.gridDataService.getTableData().subscribe(_data => {
+      this.latitude = this.gridDataService.getLatitude();
+      this.longitude = this.gridDataService.getLongitude();
+      this.rotation = -1 * this.gridDataService.getRotation();
+      this.initializeMap();
+    });
   }
 
   private initializeMap() {
     this.map = new mapboxgl.Map({
-      container: 'map',
+      container: "map",
       style: this.style,
       zoom: 14,
-	    bearing: this.rotation,
-	    pitch: 0,
+      bearing: this.rotation,
+      pitch: 0,
       center: [this.latitude, this.longitude]
     });
 
     /// Add map controls
     this.map.addControl(new mapboxgl.NavigationControl());
 
-
     //// Add Marker on Click
-    this.map.on('click', (event) => {
-      const coordinates = [event.lngLat.lng, event.lngLat.lat]
+    this.map.on("click", event => {
+      const coordinates = [event.lngLat.lng, event.lngLat.lat];
       // const newMarker   = new GeoJson(coordinates, { message: this.message })
       // this.mapService.createMarker(newMarker)
-      console.log('CLICK -- TODO at coordinates:', coordinates)
-    })
-
+      console.log("CLICK -- TODO at coordinates:", coordinates);
+    });
 
     /// Add realtime firebase data on map load
-    this.map.on('load', (event) => {
-
+    this.map.on("load", event => {
       /// register source with the dummy data of 1 point
-      this.map.addSource('simData', {
-        type: 'geojson',
+      this.map.addSource("simData", {
+        type: "geojson",
         data: {
-          type: 'MultiPoint',
+          type: "MultiPoint",
           coordinates: [0, 0]
         }
       });
       /// get source
-      this.simDataSource = this.map.getSource('simData')
-      this.mobilitySimulationService.getSimulationData()
-        .subscribe(data => {
-          this.simDataSource.setData(data)
-        });
+      this.simDataSource = this.map.getSource("simData");
+      this.mobilitySimulationService.getSimulationData().subscribe(data => {
+        this.simDataSource.setData(data);
+      });
 
-           //add the point simulation layer
+      //add the point simulation layer
       this.map.addLayer({
         id: "MultiPoint",
         source: "simData",
@@ -98,7 +89,6 @@ export class MapBoxComponent implements OnInit{
           "heatmap-radius": 10
         }
       });
-      
 
       //add the custom THREE layer
       let self = this;
@@ -114,84 +104,83 @@ export class MapBoxComponent implements OnInit{
           self.threebox.update();
         }
       });
-
-
-    })
-
+    });
   }
 
   onAddThree(map: any, mbxContext: any) {
-      this.threebox = new Threebox(map, mbxContext);
-      this.threebox.setupDefaultLights();
-      console.log('this.gridDataService.gridDataCoordinates', this.gridDataService.gridDataCoordinates)
-      // adds the 3d cityscope gemoerty
-      this.threebox.addAtCoordinate(
-        this.gridDataService.gridDataCoordinates,
-        [this.latitude, this.longitude, 0],
-        {
-          preScale: 1
-        }
-      );
-      this.threeGrid = this.threebox.scene.children[0].children[1].children[0]
-    }
+    this.threebox = new Threebox(map, mbxContext);
+    this.threebox.setupDefaultLights();
+    console.log(
+      "this.gridDataService.gridDataCoordinates",
+      this.gridDataService.gridDataCoordinates
+    );
+    // adds the 3d cityscope gemoerty
+    this.threebox.addAtCoordinate(
+      this.gridDataService.gridDataCoordinates,
+      [this.latitude, this.longitude, 0],
+      {
+        preScale: 1
+      }
+    );
+    this.threeGrid = this.threebox.scene.children[0].children[1].children[0];
+  }
 
-    update_grid_from_cityio() {
-      var array_of_types_and_colors = [
-        {
-          type: "Road",
-          color: "rgb(100,100,100)",
-          height: 0
-        },
-        {
-          type: "Open Space",
-          color: "#13f797",
-          height: 0
-        },
-        {
-          type: "live",
-          color: "#007fff",
-          height: 30
-        },
-        {
-          type: "work",
-          color: "#cc28a2",
-          height: 100
-        },
-        {
-          type: "Work 2",
-          color: "#ec0868",
-          height: 50
-        }
-      ];
+  update_grid_from_cityio() {
+    var array_of_types_and_colors = [
+      {
+        type: "Road",
+        color: "rgb(100,100,100)",
+        height: 0
+      },
+      {
+        type: "Open Space",
+        color: "#13f797",
+        height: 0
+      },
+      {
+        type: "live",
+        color: "#007fff",
+        height: 30
+      },
+      {
+        type: "work",
+        color: "#cc28a2",
+        height: 100
+      },
+      {
+        type: "Work 2",
+        color: "#ec0868",
+        height: 50
+      }
+    ];
 
-      let cityIOdata = this.gridDataService.cityIOData;
-      let grid = this.threeGrid;
-      // let textHolder = Storage.threeText;
+    let cityIOdata = this.gridDataService.cityIOData;
+    let grid = this.threeGrid;
+    // let textHolder = Storage.threeText;
 
-      for (let i = 0; i < grid.children.length; i++) {
-        //cell edit
-        let thisCell = grid.children[i];
-        //clear the text obj
-        // textHolder.children[i].text = " ";
+    for (let i = 0; i < grid.children.length; i++) {
+      //cell edit
+      let thisCell = grid.children[i];
+      //clear the text obj
+      // textHolder.children[i].text = " ";
+      thisCell.position.z = 0;
+      thisCell.scale.z = 1;
+
+      if (cityIOdata.grid[i] !== -1) {
+        thisCell.material.color.set(
+          array_of_types_and_colors[cityIOdata.grid[i]].color
+        );
+        let this_cell_height =
+          array_of_types_and_colors[cityIOdata.grid[i]].height + 1;
+        thisCell.scale.z = this_cell_height;
+        thisCell.position.z = this_cell_height / 2;
+      } else {
+        // black outs the non-read pixels
         thisCell.position.z = 0;
-        thisCell.scale.z = 1;
-
-        if (cityIOdata.grid[i] !== -1) {
-          thisCell.material.color.set(
-            array_of_types_and_colors[cityIOdata.grid[i]].color
-          );
-          let this_cell_height =
-            array_of_types_and_colors[cityIOdata.grid[i]].height + 1;
-          thisCell.scale.z = this_cell_height;
-          thisCell.position.z = this_cell_height / 2;
-        } else {
-          // black outs the non-read pixels
-          thisCell.position.z = 0;
-          thisCell.material.color.set("rgb(0,0,0)");
-        }
+        thisCell.material.color.set("rgb(0,0,0)");
       }
     }
-
+  }
 
   /// Helpers
 
@@ -217,16 +206,18 @@ export class MapBoxComponent implements OnInit{
     let offsetNorth = 1300;
     //Coordinate offsets in radians
     let dLat = offsetNorth / earthRadius;
-    let dLon = offsetEast / (earthRadius * Math.cos((Math.PI * this.latitude) / 180));
+    let dLon =
+      offsetEast / (earthRadius * Math.cos((Math.PI * this.latitude) / 180));
     //OffsetPosition, decimal degrees
     return [
-    	this.latitude + (dLat * 180) / Math.PI,
-    	this.longitude + (dLon * 180) / Math.PI];
+      this.latitude + (dLat * 180) / Math.PI,
+      this.longitude + (dLon * 180) / Math.PI
+    ];
   }
 
   flyTo(data: GeoJson) {
     this.map.flyTo({
       center: data.geometry.coordinates
-    })
+    });
   }
 }
--- src/app/map/map.service.ts
+++ src/app/map/map.service.ts
@@ -1,23 +1,21 @@
-import { Injectable } from '@angular/core';
-import { environment } from '../environments/environment';
-import { Observable, of } from 'rxjs';
+import { Injectable } from "@angular/core";
+import { environment } from "../../environments/environment";
+import { Observable, of } from "rxjs";
 
-import { GeoJson } from './map';
-import * as mapboxgl from 'mapbox-gl';
+import { GeoJson } from "./map";
+import * as mapboxgl from "mapbox-gl";
 
 export const HEROES: any[] = [];
 
 @Injectable()
 export class MapService {
-
-
   // TODO: pass in service for data, such as cityIO data.
   constructor() {
-    mapboxgl.accessToken = environment.mapbox.accessToken
+    mapboxgl.accessToken = environment.mapbox.accessToken;
   }
 
   getGridData(): Observable<any[]> {
-    console.log('getGridData')
+    console.log("getGridData");
     return of(HEROES);
   }
 
@@ -33,5 +31,4 @@ export class MapService {
   // removeMarker($key: string) {
   //   return this.db.object('/markers/' + $key).remove()
   // }
-
 }
